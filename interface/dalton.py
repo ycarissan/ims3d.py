ed7c8383b2db01a564ce3204ea5fab086db29299
ycarissan <ycarissan@gmail.com>
2021-10-11 09:42:17 +0200

    temporaryly remove problematic automatic input generation in datlton

--- interface/dalton.py
+++ interface/dalton.py
@@ -13,26 +13,26 @@ def generate_daltonFile(geom, grid, logger, outdir="./", igrid=0, maxbq=50, all_
     except:
         f.write("please provide a dalton.head file\n")
 
-    if all_atoms==None:
-        all_atoms = geom.getUniqueElementsByLabels()
-        print("generate all atoms")
-    if atomtypes==None:
-        atomtypes = geom.getUniqueLabels()
-        print("generate all labels")
-
-    for attype in atomtypes:
-        if attype.capitalize()=="BQ":
-            continue
-        elif attype.capitalize()=="C":
-            charge = 6.0
-        elif attype.capitalize()=="O":
-            charge = 8.0
-        elif attype.capitalize()=="H":
-            charge = 1.0
-        nat = len(all_atoms[attype])
-        f.write("Charge={} Atoms={} Basis=6-31g\n".format(charge, nat))
-        for at in all_atoms[attype]:
-            f.write("{:4s} {:16.10f} {:16.10f} {:16.10f}\n".format(at['label'], at['x'], at['y'], at['z']))
+#    if all_atoms==None:
+#        all_atoms = geom.getUniqueElementsByLabels()
+#        print("generate all atoms")
+#    if atomtypes==None:
+#        atomtypes = geom.getUniqueLabels()
+#        print("generate all labels")
+#
+#    for attype in atomtypes:
+#        if attype.capitalize()=="BQ":
+#            continue
+#        elif attype.capitalize()=="C":
+#            charge = 6.0
+#        elif attype.capitalize()=="O":
+#            charge = 8.0
+#        elif attype.capitalize()=="H":
+#            charge = 1.0
+#        nat = len(all_atoms[attype])
+#        f.write("Charge={} Atoms={} Basis=6-31g\n".format(charge, nat))
+#        for at in all_atoms[attype]:
+#            f.write("{:4s} {:16.10f} {:16.10f} {:16.10f}\n".format(at['label'], at['x'], at['y'], at['z']))
 
 #    print(len(grid[igrid:]),maxbq)
     f.write("Charge=0 Atoms={} Basis=pointcharge\n".format(min(len(grid[igrid:]),maxbq)))
