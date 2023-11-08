5ab3fadc94f82a45260fe55c407681b00524674d
Yannick Carissan <ycarissan@gmail.com>
2023-11-08 10:13:34 +0100

    Add closing files during generation

--- interface/dalton.py
+++ interface/dalton.py
@@ -13,28 +13,6 @@ def generate_daltonFile(geom, grid, logger, outdir="./", igrid=0, maxbq=50, all_
     except:
         f.write("please provide a dalton.head file\n")
 
-#    if all_atoms==None:
-#        all_atoms = geom.getUniqueElementsByLabels()
-#        print("generate all atoms")
-#    if atomtypes==None:
-#        atomtypes = geom.getUniqueLabels()
-#        print("generate all labels")
-#
-#    for attype in atomtypes:
-#        if attype.capitalize()=="BQ":
-#            continue
-#        elif attype.capitalize()=="C":
-#            charge = 6.0
-#        elif attype.capitalize()=="O":
-#            charge = 8.0
-#        elif attype.capitalize()=="H":
-#            charge = 1.0
-#        nat = len(all_atoms[attype])
-#        f.write("Charge={} Atoms={} Basis=6-31g\n".format(charge, nat))
-#        for at in all_atoms[attype]:
-#            f.write("{:4s} {:16.10f} {:16.10f} {:16.10f}\n".format(at['label'], at['x'], at['y'], at['z']))
-
-#    print(len(grid[igrid:]),maxbq)
     f.write("Charge=0 Atoms={} Basis=pointcharge\n".format(min(len(grid[igrid:]),maxbq)))
     nbq = 0
     for at in grid[igrid:]:
@@ -43,15 +21,18 @@ def generate_daltonFile(geom, grid, logger, outdir="./", igrid=0, maxbq=50, all_
         nbq = nbq + 1
         igrid = igrid + 1
         if (nbq == maxbq):
+            f.close()
             logger.info("Batch generation : {}".format(igrid))
             generate_daltonFile(
                 geom, grid, logger, outdir=outdir, igrid=igrid, maxbq = maxbq, all_atoms=all_atoms, atomtypes=  atomtypes)
             break
+    f = open(daltonfile, "a")
     try:
         ffoot = open("dalton.foot", "r")
         for l in ffoot.readlines():
             f.write(l)
     except:
         f.write("please provide a dalton.foot file\n")
+    f.close()
     return
 
