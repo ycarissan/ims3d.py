847b967bfc8a1b36651b4faf12310d3240ef99c7
Yannick Carissan <ycarissan@gmail.com>
2023-11-08 10:35:56 +0100

    remove recursivity in dalton input generation

--- interface/dalton.py
+++ interface/dalton.py
@@ -2,37 +2,33 @@ import pymatgen
 
 def generate_daltonFile(geom, grid, logger, outdir="./", igrid=0, maxbq=50, all_atoms=None, atomtypes=None):
     print("doing {}/{}".format(igrid, len(grid)))
-    daltonfile = outdir + \
-        "input_batch_{:05d}.dal".format(igrid)
-    f = open(daltonfile, "w")
-
-    try:
-        fhead = open("dalton.head", "r")
-        for l in fhead.readlines():
-            f.write(l)
-    except:
-        f.write("please provide a dalton.head file\n")
-
-    f.write("Charge=0 Atoms={} Basis=pointcharge\n".format(min(len(grid[igrid:]),maxbq)))
-    nbq = 0
-    for at in grid[igrid:]:
-        f.write(
-            "Bq     {0[0]:16.10f} {0[1]:16.10f} {0[2]:16.10f}\n".format(at))
-        nbq = nbq + 1
-        igrid = igrid + 1
-        if (nbq == maxbq):
-            f.close()
-            logger.info("Batch generation : {}".format(igrid))
-            generate_daltonFile(
-                geom, grid, logger, outdir=outdir, igrid=igrid, maxbq = maxbq, all_atoms=all_atoms, atomtypes=  atomtypes)
-            break
-    f = open(daltonfile, "a")
-    try:
-        ffoot = open("dalton.foot", "r")
-        for l in ffoot.readlines():
-            f.write(l)
-    except:
-        f.write("please provide a dalton.foot file\n")
-    f.close()
+    grid_size = len(grid)
+    batch=[]
+    for i in range(grid_size//maxbq):
+        batch.append(grid[i*maxbq:(i+1)*maxbq])
+    remainder = grid_size%maxbq
+    if (remainder!=0):
+        batch.append(grid[len(grid)-remainder:])
+    for ibatch in range(len(batch)):
+        daltonfile = outdir + \
+                "input_batch_{:05d}.dal".format(ibatch)
+        f = open(daltonfile, "w")
+        try:
+            fhead = open("dalton.head", "r")
+            for l in fhead.readlines():
+                f.write(l)
+        except:
+            f.write("please provide a dalton.head file\n")
+        f.write("Charge=0 Atoms={} Basis=pointcharge\n".format(min(len(grid[ibatch:]),maxbq)))
+        for bq in batch[ibatch]:
+            f.write(
+                "Bq     {0[0]:16.10f} {0[1]:16.10f} {0[2]:16.10f}\n".format(bq))
+        try:
+            ffoot = open("dalton.foot", "r")
+            for l in ffoot.readlines():
+                f.write(l)
+        except:
+            f.write("please provide a dalton.foot file\n")
+        f.close()
     return
 
